47186f98e64e4f953491657912fd64bf8bfb8e7f
DreamAlone <dalone666@qq.com>
2026-02-15 13:44:09 +0800

    实现命令行接口

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,79 @@
-fn main() {
-    println!("Hello, world!");
+use std::{
+    fs::File,
+    io::{Read, stdout},
+    path::PathBuf,
+};
+
+use ::sing_config::{convert::convert_outbounds, load::lazy::LazyLoader, sing_box};
+use anyhow::{Context, bail};
+use clap::{Args, Parser, Subcommand};
+use sing_config::sing_config;
+
+#[derive(Debug, Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Debug, Subcommand)]
+enum Commands {
+    /// 将 sing-config 构建为 sing-box config
+    Build(BuildArgs),
+}
+
+#[derive(Debug, Args)]
+struct BuildArgs {
+    /// 输入文件的路径
+    path: PathBuf,
+    /// 输出文件的路径
+    #[arg(short, long)]
+    output: Option<PathBuf>,
+}
+
+fn main() -> anyhow::Result<()> {
+    let cli = Cli::parse();
+    match cli.command {
+        Commands::Build(args) => {
+            build(&args).with_context(|| format!("未能构建文件 `{}`", args.path.display()))?
+        }
+    }
+    Ok(())
+}
+
+fn build(args: &BuildArgs) -> anyhow::Result<()> {
+    let mut file = File::open(&args.path).context("未能打开文件")?;
+    let extension = args
+        .path
+        .extension()
+        .context("文件应当具有支持的扩展名")?
+        .to_string_lossy();
+    let config: sing_config::Config = match extension.as_bytes() {
+        b"toml" => {
+            let mut buf = Vec::new();
+            file.read_to_end(&mut buf).context("未能完全读取文件")?;
+            toml::from_slice(&buf).context("未能解析文件")?
+        }
+        b"json" => serde_json::from_reader(file).context("未能解析文件")?,
+        _ => bail!("不支持扩展名 `{extension}`"),
+    };
+
+    let loader = LazyLoader::new(config.providers);
+    let outbounds = convert_outbounds(config.outbounds, &loader)?;
+    let new_config = sing_box::Config {
+        outbounds: outbounds.into_values().collect(),
+        extra: config.extra,
+    };
+
+    if let Some(path) = &args.output {
+        let file =
+            File::create(path).with_context(|| format!("未能创建输出文件 `{}`", path.display()))?;
+        serde_json::to_writer_pretty(file, &new_config)
+            .with_context(|| format!("未能将结果写入文件 `{}`", path.display()))?;
+    } else {
+        let lock = stdout().lock();
+        serde_json::to_writer_pretty(lock, &new_config).context("未能将结果写入 `stdout`")?;
+    }
+
+    Ok(())
 }
--- src/sing_config/provider.rs
+++ src/sing_config/provider.rs
@@ -3,6 +3,7 @@ use std::path::PathBuf;
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
+#[serde(rename_all = "snake_case")]
 pub enum Provider {
     Path(PathBuf),
     Url(String),
